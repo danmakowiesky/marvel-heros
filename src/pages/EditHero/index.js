52bd064a7722fd277fdbad78483a18b96d5eed14
danielmakowiesky <dvm1993@hotmail.com>
2020-06-20 13:48:20 -0300

    retirando dependencia do codigo que nao e usada

--- src/pages/EditHero/index.js
+++ src/pages/EditHero/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import { editHero } from '../../store/modules/main/actions';
 
